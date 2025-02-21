3632fb03f11e3637424ae4a2cb5fb4f35da325cc
jamu85 <jamu85@icloud.com>
2025-02-21 10:52:56 +0800

    Adds kubeconfig to fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,10 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting 
+    
     # atuin
     set -gx ATUIN_NOBIND "true"
     atuin init fish | source
     bind \cr _atuin_bind_up
+
+    # kube
+    set -gx KUBECONFIG "$HOME/.kube/config:$HOME/.kube/prod_config"
+
+    # direnv
+    direnv hook fish | source
 
     # starship
     starship init fish | source
